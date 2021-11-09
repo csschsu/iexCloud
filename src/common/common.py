03b17713d1297c6f6b778ec0c5478c5ceb4d387e
csschsu <christer.sundgren@systemkonstruktion.se>
2021-11-09 16:45:34 +0100

    check datafile exists before download

--- src/common/common.py
+++ src/common/common.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 def resolveQueryParams ( query : str, symbol : str, date:str ) :
@@ -8,10 +9,12 @@ def resolveQueryParams ( query : str, symbol : str, date:str ) :
 
 def writeresultfile ( data :str, symbol :str, date : str ):
     res_json = json.loads(data)
-    # print(json.dumps(res_json, indent=4, sort_keys=True))
-    filename = symbol + "_" + date + ".dat"
-    with open(filename, 'w') as f:
+    with open(resultfilename( symbol, date), 'w') as f:
         s = json.dumps(res_json, indent=2)
         f.write('%s\n' % (s))
         f.close()
+def resultfilename (symbol :str, date : str) :
+    return "../data/" + symbol + "_" + date + ".dat"
 
+def checkresultfile ( symbol :str, date : str ):
+    return os.path.isfile(resultfilename( symbol, date))
--- src/timeSeriesClient.py
+++ src/timeSeriesClient.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 from common import iexDate, config
-from common.common import resolveQueryParams, writeresultfile
+from common.common import resolveQueryParams, writeresultfile, checkresultfile
 import requests
 
 env = config.Environment ()
 
+
 for symbol in env.symbols :
+    if checkresultfile(symbol, iexDate.previousworkday()) :
+        continue
     uri = env.iexBase + "/stock/<INSERT_SYMBOL>/chart/date/<INSERT_DATE>" + env.token
     uri = resolveQueryParams(uri, symbol, iexDate.previousworkday())
     print ("Query uri :" + uri )
