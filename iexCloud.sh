8ea338820586625b193f15922e8781ece2aec946
csschsu <christer.sundgren@systemkonstruktion.se>
2021-11-09 18:49:27 +0100

    included path

--- iexCloud.sh
+++ iexCloud.sh
@@ -1,3 +1,4 @@
-mkdir data
-cd src
-/home/christer/iexCloud/src/timeSeriesClient.py
+IEXHOME=/home/christer/iexCloud
+mkdir ${IEXHOME}/datai 2>/dev/null
+cd ${IEXHOME}/src
+${IEXHOME}/src/timeSeriesClient.py > ${IEXHOME}/out.log
